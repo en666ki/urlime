7bb04bbbe2784207f6eb46fa174705eb087ce554
en666ki <en666ki@gmail.com>
2023-08-07 17:37:05 +0000

    added to main for handle test

--- cmd/lime/main.go
+++ cmd/lime/main.go
@@ -4,13 +4,12 @@ import (
 	"log"
 
 	"github.com/en666ki/urlime/pkg/server"
+	"github.com/en666ki/urlime/pkg/shorten"
 )
 
 func main() {
 	var s server.Server
-	s.AddHandler("POST", "/test", func(body []byte) ([]byte, error) {
-		return body, nil
-	})
+	s.AddHandler("POST", "/shorten", shorten.Shorten)
 	err := s.Start("8080")
 	if err != nil {
 		log.Fatalf("Server was crushed with error: %v", err)
